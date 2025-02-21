e5560b87405ce8236ca48d08c79fded6bd8bffcf
Michael J Sigg <mjsigg@gmail.com>
2025-02-20 21:20:07 -0600

    init new project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col h-screen bg-yellow-300 justify-center items-center">
+        <p>p tag</p>
+        <h1>h1 test</h1>
+        <h1>h2 test</h1>
+        <i className="fa-solid fa-image"></i>
+        <i className="fa-regular fa-circle-user"></i>
+        <i class="fa-solid fa-user"></i>
+      </div>
+    </>
+  );
+}
+
+export default App;
